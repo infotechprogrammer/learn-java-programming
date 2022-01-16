247bae9822da5f2460d790c6d442949bd6872d2f
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2022-01-16 19:09:37 +0530

    Update _16_Find_Highest_Altitude.java

--- _21_Data_Structure_Problems/_03_Array_Problems/_16_Find_Highest_Altitude.java
+++ _21_Data_Structure_Problems/_03_Array_Problems/_16_Find_Highest_Altitude.java
@@ -1,4 +1,3 @@
-package com.Array_Problems;
 /*
 There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes.
 The biker starts his trip on point 0 with altitude equal 0.
