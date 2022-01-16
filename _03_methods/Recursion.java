709357d4cf58fa0ffb5b007ed289c31a370677b9
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2022-01-16 18:37:26 +0530

    Update Recursion.java

--- _03_methods/Recursion.java
+++ _03_methods/Recursion.java
@@ -1,4 +1,3 @@
-package com._03_methods;
 //Adding two numbers together is easy to do, but adding a range of numbers is more complicated. In the following example, recursion is used to add a range of numbers together by breaking it down into the simple task of adding two numbers:
 public class Recursion {
     public static void main(String[] args) {
