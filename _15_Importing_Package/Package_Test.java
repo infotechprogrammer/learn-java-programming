02f2b017c79818db0a0525045b964cdb9b53b9e6
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2022-01-16 18:44:11 +0530

    Update Package_Test.java

--- _15_Importing_Package/Package_Test.java
+++ _15_Importing_Package/Package_Test.java
@@ -1,6 +1,4 @@
-package com._15_Importing_Package;
-
-import com._14_Making_Package.ClassA;
+import data-structures-in-java._14_Making_Package.ClassA;
 
 public class Package_Test {
     public static void main(String[] args) {
