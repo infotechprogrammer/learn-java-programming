49daf3d03b73057954c40c3c025a385965cc569f
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2022-01-16 19:08:01 +0530

    Update _13_Smaller_Number_Than_Current_Number.java

--- _21_Data_Structure_Problems/_03_Array_Problems/_13_Smaller_Number_Than_Current_Number.java
+++ _21_Data_Structure_Problems/_03_Array_Problems/_13_Smaller_Number_Than_Current_Number.java
@@ -1,4 +1,3 @@
-package com.Array_Problems;
 /*
 Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
 That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
