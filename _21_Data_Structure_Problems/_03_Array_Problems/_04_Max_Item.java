f0497e2c038e844ee8ad0421e6b8a7bf2d9c5438
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2022-01-16 18:30:27 +0530

    Add files via upload

---
+++ _21_Data_Structure_Problems/_03_Array_Problems/_04_Max_Item.java
@@ -0,0 +1,37 @@
+package com.Array_Problems;
+
+public class _04_Max_Item {
+    public static void main(String[] args) {
+        int[] arr = {1,4,18,90,180,36,68,32};
+        System.out.print("Maximum Value: "+max(arr));
+        System.out.println();
+        System.out.print("Maximum Value between indexes [1] and [3]: "+maxRange(arr,1,3));
+    }
+    static int max(int[] arr) {
+        if(arr.length == 0){
+            return -1;
+        }
+        int maxVal = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+    static int maxRange(int[] arr, int start, int end) {
+        if(end < start) {
+            return -1;
+        }
+        if(arr == null) {
+            return -1;
+        }
+        int maxVal = arr[start];
+        for (int i = start; i <= end; i++) {
+            if(arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+}
---
+++ _21_Data_Structure_Problems/_03_Array_Problems/_21_Add_Integer_to_Array.java
@@ -0,0 +1,43 @@
+package com.Array_Problems;
+/*
+The array-form of an integer num is an array representing its digits in left to right order.
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+
+Example 1:
+Input: num = [1,2,0,0], k = 34
+Output: [1,2,3,4]
+Explanation: 1200 + 34 = 1234
+
+Example 2:
+Input: num = [2,7,4], k = 181
+Output: [4,5,5]
+Explanation: 274 + 181 = 455
+ */
+import java.util.*;
+public class _21_Add_Integer_to_Array {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] nums = {1,2,0,0};
+        System.out.print("Original Array: "+ Arrays.toString(nums));
+        System.out.println();
+        System.out.print("Enter number you want to add: ");
+        int num = in.nextInt();
+        System.out.println();
+        System.out.println("Array after Addition = "+addToArrayForm(nums,num));
+    }
+    static ArrayList<Integer> addToArrayForm(int[] num, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = num.length-1; i>=0 || k>0; i--) {
+            if(i>=0) {
+                list.add((num[i]+k)%10);
+                k=(num[i]+k)/10;
+            } else {
+                list.add(k%10);
+                k/=10;
+            }
+        }
+        Collections.reverse(list);
+        return list;
+    }
+}
