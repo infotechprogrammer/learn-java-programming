e107f196af5a57719870c8d1f145ebb74f7c518b
Sahil Batra <78542194+infotechprogrammer@users.noreply.github.com>
2022-01-16 19:10:44 +0530

    Update _24_Lucky_Number_in_Matrix.java

--- _21_Data_Structure_Problems/_03_Array_Problems/_24_Lucky_Number_in_Matrix.java
+++ _21_Data_Structure_Problems/_03_Array_Problems/_24_Lucky_Number_in_Matrix.java
@@ -1,4 +1,3 @@
-package com.Array_Problems;
 /*
 Given an m x n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
 A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
